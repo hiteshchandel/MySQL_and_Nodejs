00ea15576dc51d588c1cea5ebfd54e405c2666d4
Hitesh Chandel <hiteshchandel858@gmail.com>
2025-08-26 17:36:27 +0530

    add update controller

--- user CRUD using MySQL ORM/controller.js
+++ user CRUD using MySQL ORM/controller.js
@@ -32,3 +32,20 @@ exports.getUserById = async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
+
+exports.updateUser = async (req, res) => {
+    try {
+        // console.log('Updating user with id:', req.params.id);
+        const user = await User.findByPk(req.params.id);
+        // console.log('Found user:', user);
+        if (!user) {
+            console.log('User not found with id:', req.params.id);
+            return res.status(404).json({ error: 'User not found' });
+        }
+        await user.update(req.body);
+        res.status(200).json(user);
+    } catch (error) {
+        console.log('Error updating user:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
--- user CRUD using MySQL ORM/router.js
+++ user CRUD using MySQL ORM/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createUser, getAllUsers, getUserById } = require('./controller');
+const { createUser, getAllUsers, getUserById, updateUser } = require('./controller');
 const router = express.Router();
 
 
 router.post('/user', createUser);
 router.get('/user/all', getAllUsers);
 router.get('/user/:id', getUserById);
+router.put('/user/:id', updateUser);
 
 module.exports = router;
